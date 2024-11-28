b8efb796fad80f2e1d18b508d7d600cab492374d
jinzhiguang <jinzhiguang@kylinos.cn>
2024-11-28 10:41:35 +0800

    split ebpf code into separate directory

---
+++ iscsi_bpf/iscsi_stats_ebpf.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) KylinSoft Co., Ltd. 2024-2025.All rights reserved.
+ * storprototrace is licensed under the Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *         http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PSL v2 for more details.
+ */
+#ifndef ISCSI_STATS_EBPF_H
+#define ISCSI_STATS_EBPF_H
+
+#include "common.h"
+
+bool iscsi_stats_ebpf_load_and_attach();
+bool iscsi_stats_ebpf_loop(void(*handle)(struct iscsi_stats *stats));
+
+#endif
